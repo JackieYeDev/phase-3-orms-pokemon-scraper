b2da700896104ef379428fa7f351fa82cd8403d9
Jackie <jye0325@gmail.com>
2022-08-02 17:15:38 -0400

    commit tree 6c9ef6a4588f2c0d3f12bffb26f9854565ee5fd5

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+#    def initialize(options={}) 
+#         options.each do |property, value|
+#             self.send("#{property}=", value)
+#         end
+#    end
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save (name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(num, db)
+        pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", num).flatten
+        Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    end
 end
